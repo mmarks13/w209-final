0b30ef599769430f2b6522fdeea8355ce174b8c2
root <root@w209db.krishnaswami.org>
2016-04-14 20:24:27 -0500

    api webservice updates

--- api/api.py
+++ api/api.py
@@ -1,48 +1,133 @@
 #!/usr/bin/python
-from flask import Flask, Response
+from flask import Flask, Response,  request, send_from_directory
+from flask.ext.cors import CORS
+from flask_limiter import Limiter
+
 import ujson as json
 import logging
+import gdal
 import sqlalchemy
+from geopy.geocoders import Nominatim
+import pyproj
 
 app=Flask('w209dbapi')
+CORS(app)
+limiter = Limiter(
+    app,
+    key_func=lambda: '1' # for all users
+)
 engine = sqlalchemy.create_engine('mysql://reader@localhost/final', echo=True)
 logger = logging.getLogger('w209dbapi')
+geolocator=Nominatim(country_bias='US', user_agent="student final project: UC-B MIDS W209: Data Visualization contact: nk@ischool.berkeley.edu")
 
-def generate(cursor):
+
+@app.route('/')
+def root():
+        return Response(
+            ["Go away"],
+            mimetype='text/plain')
+
+
+def generate(query):
     err=''
-    keys=cursor.keys()
-    yield '{"keys":'+json.dumps(keys)+',"rows":['
+    yield '{"data": ['
     try:
-        first=True
-        for row in cursor:
-            if first:
-                pfx=''
-                first=False
-            else:
-                pfx=','
-            yield  pfx+json.dumps(row.values())
+        cursor=engine.execute(query)
+        pfx=''
+        while True:
+            rows=cursor.fetchmany()
+            if not rows:
+                break
+            for row in rows:
+                yield  pfx+json.dumps(dict(row.items()))
+                if not pfx:
+                    pfx=','
     except Exception,e:
         err=',"error":' + json.dumps(str(e))
     finally:
         yield ']'+err+'}\n'
-@app.route('/final/<query>')
-def doQuery(query):
+
+@app.route('/sql')
+def doQuery():
     try:
-        q=query.strip()
-        if not q.endswith(';'):
-            q+=';'
-        cursor=engine.execute(q);
-        logger.info('Returning {0} rows for query {0}'.format(
-            cursor.rowcount,
-            q
-        ))
-        return Response(generate(cursor), mimetype='application/json')
+        query=request.values['query'].strip()
+        if not query.endswith(';'):
+            query+=';'
+        return Response(generate(query), mimetype='application/json')
     except Exception, e:
         return Response(
             [json.dumps({'error': str(e)}), '\n'],
             status='500 Server exception',
             mimetype='application/json')
 
+def fakeQuery(filename):
+    return Response(
+        '{'+'"data":{data},"err":""'.format(
+            data=open(filename).read())+'}',
+        status='200 OK',
+        mimetype='application/json')
+
+
+@app.route('/mainTable/')
+@app.route('/mainTable/<physician>')
+def mainTable(physician=None):
+    if 'real' not in request.values:
+        return fakeQuery('Main_Table_Lens_Data.json');
+    if physician:
+        where='WHERE PhysicianProfileID={physician}'.format(physician=physician)
+    else:
+        where=''
+    return doQuery(
+'''SELECT 
+    DrugName,
+    NameOfAssociatedCoveredDrugOrBiological1 as RxBrand,
+    NDCOfAssociatedCoveredDrugOrBiological1  as RxNDC,
+    TotalClaimCountAgg                       as RxCount,
+    NumberOfPaymentsIncludedInTotalAmountAgg as PaymentCount,
+    AmountOfPaymentUSDollarsAgg              as PaymentTotal
+FROM OpenPaymentPrescrJoin4
+GROUP BY PhysicianProfileID
+{where};'''.format(where=where))
+
+
+@app.route('/hoverTable/<physician>')
+def hoverTable(physician):
+    return fakeQuery('Hover_Table_Lens_Data.json');
+    return doQuery(
+'''SELECT 
+    DrugName,
+    NameOfAssociatedCoveredDrugOrBiological1 as RxBrand,
+    NDCOfAssociatedCoveredDrugOrBiological1  as RxNDC,
+    TotalClaimCountAgg                       as RxCount,
+    NumberOfPaymentsIncludedInTotalAmountAgg as PaymentCount,
+    AmountOfPaymentUSDollarsAgg              as PaymentTotal
+FROM OpenPaymentPrescrJoin4
+WHERE PhysicianProfileID={physician} ;'''.format(physician=physician))
+
+
+
+
+# This call enforces a rate limit of 1 per second to conform to OSM
+# Nominatim terms of use.  UI should restrict subsequent calls based
+# on the Retry-After response header, and can see the rate limit
+# params in X-RateLimit-* headers
+@limiter.shared_limit('1 per second', 'nominatim')
+@app.route('/locate/<loc>')
+def locate(loc):
+    return Response(
+        [json.dumps(geolocator.geocode(loc, geometry='wkt'))],
+        status='203 Non-Authoritative Information from OSM Nominatim',
+        mimetype='application/json')
+    
+@limiter.shared_limit('1 per second', 'nominatim')
+@app.route('/near/<lat>/<lon>/<radius>')
+def near(lat, lon, distance):
+    doQuery("""SELECT zip,ST_ASTEXT(loc)
+FROM final.ZipLoc 
+WHERE ST_WITHIN(loc, ST_BUFFER(POINT({lat},{lon}), {degs};"""
+            .format(lat=lat, lon=lon, degs=radius/69))
+
+
 if __name__=='__main__':
     engine.connect()
-    app.run(host='0.0.0.0', port=20900)
+    app.run(host='0.0.0.0', port=20900, debug=True)
